24341733c2fabc4a01e73bda194e419f300ad068
Rajashree Parhi <rajashree.parhi@factset.com>
2022-05-26 00:56:29 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+      <p style={{color:"rgb(190,255,0)"}}>Profile currently under construction! </p>
+      <p style={{color:"rgb(190,255,0)"}}> Please check back again later.</p>
     </div>
   );
 }
